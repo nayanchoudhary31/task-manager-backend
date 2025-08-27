1d73b53ca47295ab3bd294d48331dcd8a192f4a0
nayanchoudhary31 <nayanscsit09@gmail.com>
2025-08-27 19:47:40 +0530

    refactor: convert task controller methods to async/await for improved asynchronous handling

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,28 +1,28 @@
 const taskModel = require("../models/taskModel");
 
-const getTasks = (req, resp, next) => {
+const getTasks = async (req, resp, next) => {
   try {
-    const tasks = taskModel.getAllTasks();
+    const tasks = await taskModel.getAllTasks();
     resp.status(200).json({ tasks: tasks });
   } catch (error) {
     next(error);
   }
 };
 
-const getTaskById = (req, resp, next) => {
+const getTaskById = async (req, resp, next) => {
   try {
     const taskId = req.taskId;
-    const task = taskModel.getTaskById(taskId);
+    const task = await taskModel.getTaskById(taskId);
     resp.status(200).json({ task: task });
   } catch (error) {
     next(error);
   }
 };
 
-const addTask = (req, resp, next) => {
+const addTask = async (req, resp, next) => {
   try {
     let { taskName } = req.body;
-    const newTask = taskModel.createTask(taskName);
+    const newTask = await taskModel.createTask(taskName);
 
     resp.status(201).json({
       message: "Task Added Successfully!",
@@ -33,11 +33,14 @@ const addTask = (req, resp, next) => {
   }
 };
 
-const updateTask = (req, resp, next) => {
+const updateTask = async (req, resp, next) => {
   try {
     const taskId = req.taskId;
     const { taskName, isCompleted } = req.body;
-    const updatedTask = taskModel.updateTask(taskId, { taskName, isCompleted });
+    const updatedTask = await taskModel.updateTask(taskId, {
+      taskName,
+      isCompleted,
+    });
     resp.status(200).json({
       message: "Task Updated Successfully!",
       task: updatedTask,
@@ -47,10 +50,10 @@ const updateTask = (req, resp, next) => {
   }
 };
 
-const deleteTask = (req, resp, next) => {
+const deleteTask = async (req, resp, next) => {
   try {
     const taskId = req.taskId;
-    const task = taskModel.deleteTask(taskId);
+    const task = await taskModel.deleteTask(taskId);
     resp
       .status(200)
       .json({ message: "Task Deleted Successfully!", task: task });
