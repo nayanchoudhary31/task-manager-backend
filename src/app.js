7b29446f379a447c0f8927133305065c153cdd94
nayanchoudhary31 <nayanscsit09@gmail.com>
2025-08-27 19:47:30 +0530

    refactor: replace body-parser with built-in express.json() middleware for JSON parsing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const taskRoutes = require("./routes/taskRoutes");
-const bodyParser = require("body-parser");
 const requestLogger = require("./middlewares/requestLogger");
 const errorHanlder = require("./middlewares/errorHandler");
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(requestLogger);
 
 app.use("/api/", taskRoutes);
